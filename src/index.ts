3fe332a8e07960fca3cf9490f9d5eaf97bd70c00
pmqueiroz <pedroqueiroz593@gmail.com>
2023-03-09 21:37:09 -0300

    fix: entry file

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
 import { inspectModule } from './core/inspect-module'
 
-const mockEntry = `\
-import a from 'b'
-import { c } from 'd'
-import { e as f } from 'g'
-`
-
-;(async () => {
-  const result = await inspectModule(mockEntry)
-
-  console.log(result)
-})()
+export { inspectModule }
