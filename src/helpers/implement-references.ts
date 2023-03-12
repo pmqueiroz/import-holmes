0ee5c83976eeff5d6baf4f9e7015010b85a3b421
Pedro Queiroz <pedroqueiroz593@gmail.com>
2023-03-12 14:15:35 -0300

    feat: export typings (#13)
    * refactor: remove already done todo
    
    * refactor: adjusts typings
    
    * feat: export typings
    
    * refactor: rollback
    
    * refactor: rollback async

--- src/helpers/implement-references.ts
+++ src/helpers/implement-references.ts
@@ -2,7 +2,10 @@ import { Program } from '@swc/core'
 import { ImportHolmesInspect } from '../types'
 import { CountImportSpecifierVisitor } from './count-import-specifier-visitor'
 
-export const implementReferences = (inspect: ImportHolmesInspect, ast: Program) => {
+export const implementReferences = (
+  inspect: Omit<ImportHolmesInspect, 'referenced'>,
+  ast: Program
+): ImportHolmesInspect => {
   const visitor = new CountImportSpecifierVisitor(inspect.specifier)
   visitor.visitProgram(ast)
 
