2431c6d7919e1e7cddc35d6f502471c44ec4d3db
Pedro Queiroz <pedroqueiroz593@gmail.com>
2023-03-12 23:06:40 -0300

    fix: missing references (#19)

--- src/helpers/count-import-specifier-visitor.ts
+++ src/helpers/count-import-specifier-visitor.ts
@@ -1,4 +1,4 @@
-import { Identifier, TsType } from '@swc/core'
+import { Identifier, JSXClosingElement, TsType } from '@swc/core'
 import { Visitor } from '@swc/core/Visitor'
 
 export class CountImportSpecifierVisitor extends Visitor {
@@ -8,13 +8,10 @@ export class CountImportSpecifierVisitor extends Visitor {
   constructor(specifierName: string) {
     super()
     this.specifier = specifierName
-    /**
-     * @disclaimer starts negative to bypass import id itself
-     */
-    this.count = -1
+    this.count = 0
   }
 
-  visitIdentifier(id: Identifier): Identifier {
+  visitIdentifierReference(id: Identifier): Identifier {
     if (id.value === this.specifier) {
       this.count++
     }
@@ -22,7 +19,30 @@ export class CountImportSpecifierVisitor extends Visitor {
     return id
   }
 
-  visitTsType(n: TsType): TsType {
-    return n
+  visitJSXClosingElement(id: JSXClosingElement | undefined): JSXClosingElement | undefined {
+    /**
+     * @disclaimer just for add this method here it deduplicates the identifier count on closing JSX
+     * */
+
+    return id
+  }
+
+  /**
+   * @fix there still some types that aren't caught
+   */
+  visitTsType(id: TsType): TsType {
+    if (
+      (id.type === 'TsTypeReference' &&
+        id.typeName.type === 'Identifier' &&
+        id.typeName.value === this.specifier) ||
+      (id.type === 'TsIndexedAccessType' &&
+        id.objectType.type === 'TsTypeReference' &&
+        id.objectType.typeName.type === 'Identifier' &&
+        id.objectType.typeName.value === this.specifier)
+    ) {
+      this.count++
+    }
+
+    return id
   }
 }
