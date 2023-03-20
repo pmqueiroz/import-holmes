929004dda626a0b7688036586a91e20081a25418
pmqueiroz <pedroqueiroz593@gmail.com>
2023-03-20 00:14:56 -0300

    feat: implement references

--- crates/inspect_module/src/main.rs
+++ crates/inspect_module/src/main.rs
@@ -10,13 +10,17 @@ fn main() {
     let source_code = r#"
         import { useState } from 'react'
         import React from 'react'
-        import { useState as useCleiton } from 'react'
+        import { useRef as useCleiton } from 'react' 
+
+        const cleiton = () => {
+            const [catapimbas, cataporas] = useState()
+        }
     "#;
 
     let program = parser::parse_program(source_code);
     let inspects = visitor::get_program_inspects(program);
     
     for inspect in inspects {
-        println!("specifier: {} module_name: {}", inspect.specifier, inspect.module_name);
+        println!("specifier: {} module_name: {} referenced: {}", inspect.raw.specifier, inspect.raw.module_name, inspect.referenced);
     }
 }
