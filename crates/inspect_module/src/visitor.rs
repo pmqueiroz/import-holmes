0797afcd8bde367d7f47ddb9defd75a1b8ace3ac
pmqueiroz <pedroqueiroz593@gmail.com>
2023-03-20 00:14:56 -0300

    feat: implement references

--- crates/inspect_module/src/visitor.rs
+++ crates/inspect_module/src/visitor.rs
@@ -1,29 +1,47 @@
+use std::collections::HashMap;
 use swc_ecma_ast::*;
 use swc_ecma_visit::Visit;
 
 #[derive(Debug)]
-pub struct Inspect {
+pub struct RawInspect {
    pub specifier: String,
    pub module_name: String
 }
 
-struct ImportVisitor {
-    inspects: Vec<Inspect>
+pub struct  Inspect {
+    pub raw: RawInspect,
+    pub referenced: usize
 }
+
+struct ImportVisitor {
+    raw_inspects: Vec<RawInspect>,
+    reference_counts: HashMap<String, usize>
+}
+
 
 impl Visit for ImportVisitor {
     fn visit_module_item(&mut self, item: &ModuleItem) {
         if let ModuleItem::ModuleDecl(ModuleDecl::Import(import_decl)) = item {
             let inspect = generate_inspect(import_decl);
-            self.inspects.extend(inspect);
+            self.raw_inspects.extend(inspect);
         }
 
         swc_ecma_visit::visit_module_item(self, item);
     }
+
+    fn visit_ident(&mut self, ident: &Ident) {
+        if let Some(count) = self.reference_counts.get_mut(&ident.sym.to_string()) {
+            *count += 1;
+        } else {
+            self.reference_counts.insert(ident.sym.to_string(), 1);
+        }
+
+        swc_ecma_visit::visit_ident(self, ident);
+    }
 }
 
-fn generate_inspect(node: &ImportDecl) -> Vec<Inspect> {
-    let mut inspects = Vec::new();
+fn generate_inspect(node: &ImportDecl) -> Vec<RawInspect> {
+    let mut raw_inspects = Vec::new();
 
     let statements = node.specifiers.iter().map(|specifier| {
         match specifier {
@@ -39,19 +57,19 @@ fn generate_inspect(node: &ImportDecl) -> Vec<Inspect> {
                     }
                 })
                         .unwrap_or_else(|| named_specifier.local.sym.to_string());
-                Inspect {
+                RawInspect {
                     specifier: imported_value,
                     module_name: node.src.value.to_string(),
                 }
             }
             ImportSpecifier::Default(default_specifier) => {
-                Inspect {
+                RawInspect {
                     specifier: default_specifier.local.sym.to_string(),
                     module_name: node.src.value.to_string(),
                 }
             }
             ImportSpecifier::Namespace(_) => {
-                Inspect {
+                RawInspect {
                     specifier: "*".to_string(),
                     module_name: node.src.value.to_string(),
                 }
@@ -59,17 +77,34 @@ fn generate_inspect(node: &ImportDecl) -> Vec<Inspect> {
         }
     });
 
-    inspects.extend(statements);
+    raw_inspects.extend(statements);
 
-    inspects
+    raw_inspects
 }
 
 pub fn get_program_inspects(program: Program) -> Vec<Inspect> {
    let mut visitor = ImportVisitor {
-    inspects: Vec::new()
+        raw_inspects: Vec::new(),
+        reference_counts: HashMap::new(),
    };
 
    visitor.visit_program(&program);
 
-   return visitor.inspects;
+   let mut inspects = Vec::new();
+
+   for raw_inspect in visitor.raw_inspects {
+        let referenced = visitor.reference_counts.get(&raw_inspect.specifier)
+            // subtract 1 to skip self import ident
+            .map_or(0, |value| value - 1);
+
+        inspects.push(
+            Inspect {
+                raw: raw_inspect,
+                referenced
+            }
+        )
+   }
+
+   inspects
 }
+    
