bbd2a0eb18319c9bb3b3cd71339882f34c3e6fc6
pmqueiroz <pedroqueiroz593@gmail.com>
2023-03-21 01:01:18 -0300

    feat: improve glob matching

--- crates/inspect_cli/src/main.rs
+++ crates/inspect_cli/src/main.rs
@@ -16,9 +16,9 @@ fn main() {
     let args = Args::parse();
     let cwd = env::current_dir().unwrap();
     // implement module filter in core
-    let _package = read_module::read_package_json(cwd);
+    let _package = read_module::read_package_json(&cwd);
     
-    let files = read_module::get_module_files(args.glob);
+    let files = read_module::get_module_files(&cwd, args.glob);
 
     let inspects: Vec<inspect_core::Inspect> = files
         .par_iter()
