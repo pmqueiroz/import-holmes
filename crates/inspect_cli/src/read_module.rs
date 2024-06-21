08d9dfb9f37ba7f322e87191a8b9b9696082ee27
pmqueiroz <pedroqueiroz593@gmail.com>
2024-06-21 13:36:04 -0300

    style: add rustfmt

--- crates/inspect_cli/src/read_module.rs
+++ crates/inspect_cli/src/read_module.rs
@@ -2,64 +2,65 @@ extern crate globwalk;
 
 use serde::{Deserialize, Serialize};
 use serde_json;
-use std::path::PathBuf;
 use std::collections::HashMap;
+use std::path::PathBuf;
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Package {
-   pub dependencies: HashMap<String, String>,
-   #[serde(rename = "devDependencies")]
-   pub dev_dependencies: HashMap<String, String>
+  pub dependencies: HashMap<String, String>,
+  #[serde(rename = "devDependencies")]
+  pub dev_dependencies: HashMap<String, String>,
 }
 
 pub fn package_exists(dir_path: &PathBuf) -> bool {
-   let package_json_path = dir_path.join("package.json");
-   package_json_path.exists()
+  let package_json_path = dir_path.join("package.json");
+  package_json_path.exists()
 }
 
 pub fn read_package_json(cwd: &PathBuf) -> Package {
-   let file_path = cwd.join("package.json");
+  let file_path = cwd.join("package.json");
 
-   let file = std::fs::File::open(file_path).expect("Failed to read package.json file");
+  let file =
+    std::fs::File::open(file_path).expect("Failed to read package.json file");
 
-   let data: Package = serde_json::from_reader(file).expect("Failed to read package.json file");
+  let data: Package =
+    serde_json::from_reader(file).expect("Failed to read package.json file");
 
-   data
+  data
 }
 
-pub fn get_module_files(cwd: &PathBuf, arg_glob: Option<String>) -> Vec<String> {
-   let mut paths: Vec<String> = Vec::new();
+pub fn get_module_files(
+  cwd: &PathBuf,
+  arg_glob: Option<String>,
+) -> Vec<String> {
+  let mut paths: Vec<String> = Vec::new();
 
-   let glob_pattern = arg_glob.unwrap_or("**/*.{ts,tsx}".to_string());
-   let mut patterns: Vec<String> = vec![glob_pattern];
+  let glob_pattern = arg_glob.unwrap_or("**/*.{ts,tsx}".to_string());
+  let mut patterns: Vec<String> = vec![glob_pattern];
 
-   let mut ignore_patterns = vec![
-      "node_modules/*".to_string(),
-      "**/*.{spec,test}.{ts,tsx}".to_string(),
-      "**/*.d.ts".to_string(),
-   ];
+  let mut ignore_patterns = vec![
+    "node_modules/*".to_string(),
+    "**/*.{spec,test}.{ts,tsx}".to_string(),
+    "**/*.d.ts".to_string(),
+  ];
 
-   ignore_patterns
-      .iter_mut()
-      .for_each(|s| s.insert(0, '!'));
+  ignore_patterns.iter_mut().for_each(|s| s.insert(0, '!'));
 
-   patterns.extend(ignore_patterns);
+  patterns.extend(ignore_patterns);
 
-   let glob_paths: Vec<globwalk::DirEntry> = globwalk::GlobWalkerBuilder::from_patterns(
-      cwd.clone(), 
-      &patterns
-   )
+  let glob_paths: Vec<globwalk::DirEntry> =
+    globwalk::GlobWalkerBuilder::from_patterns(cwd.clone(), &patterns)
       .build()
       .unwrap()
       .into_iter()
       .filter_map(Result::ok)
-      .collect(); 
+      .collect();
 
-   for path in glob_paths {
-      if let Some(pathname) = path.path().to_str() {
-         paths.push(pathname.to_string());
-      }
-   }
+  for path in glob_paths {
+    if let Some(pathname) = path.path().to_str() {
+      paths.push(pathname.to_string());
+    }
+  }
 
-   paths
+  paths
 }
--- crates/inspect_core/src/lib.rs
+++ crates/inspect_core/src/lib.rs
@@ -1,6 +1,6 @@
 extern crate swc_common;
-extern crate swc_ecma_parser;
 extern crate swc_ecma_ast;
+extern crate swc_ecma_parser;
 extern crate swc_ecma_visit;
 
 use std::collections::HashMap;
@@ -12,62 +12,66 @@ pub use visitor::Inspect;
 use visitor::RawInspect;
 
 pub enum SortBy {
-    Referenced,
-    Occurrences,
-    None
+  Referenced,
+  Occurrences,
+  None,
 }
 
 pub fn inspect_module(source_code: &str) -> Vec<visitor::Inspect> {
-    let program = parser::parse_program(source_code);
-    let inspects = visitor::get_program_inspects(program);
+  let program = parser::parse_program(source_code);
+  let inspects = visitor::get_program_inspects(program);
 
-    inspects
+  inspects
 }
 
 pub fn dedupe_inspects(inspects: Vec<Inspect>) -> Vec<Inspect> {
-    let mut merged_map: HashMap<(String, String), (usize, usize)> = HashMap::new();
+  let mut merged_map: HashMap<(String, String), (usize, usize)> =
+    HashMap::new();
 
-    for inspect in inspects {
-        let key = (
-            inspect.raw.specifier.clone(),
-            inspect.raw.module_name.clone(),
-        );
-        let (total_referenced, total_occurrences) = merged_map.entry(key).or_insert((0, 0));
-        *total_referenced += inspect.referenced;
-        *total_occurrences += inspect.occurrences;
-    }
+  for inspect in inspects {
+    let key = (
+      inspect.raw.specifier.clone(),
+      inspect.raw.module_name.clone(),
+    );
+    let (total_referenced, total_occurrences) =
+      merged_map.entry(key).or_insert((0, 0));
+    *total_referenced += inspect.referenced;
+    *total_occurrences += inspect.occurrences;
+  }
 
-    let merged_inspects: Vec<Inspect> = merged_map
-        .into_iter()
-        .map(|((specifier, module_name), (referenced, occurrences))| Inspect {
-            raw: RawInspect {
-                specifier,
-                module_name,
-            },
-            referenced,
-            occurrences,
-        })
-        .collect();
+  let merged_inspects: Vec<Inspect> = merged_map
+    .into_iter()
+    .map(
+      |((specifier, module_name), (referenced, occurrences))| Inspect {
+        raw: RawInspect {
+          specifier,
+          module_name,
+        },
+        referenced,
+        occurrences,
+      },
+    )
+    .collect();
 
-    merged_inspects
+  merged_inspects
 }
 
 pub fn sort_by(inspects: Vec<Inspect>, by: SortBy) -> Vec<Inspect> {
-    match by {
-        SortBy::Referenced => sort_by_referenced(inspects),
-        SortBy::Occurrences => sort_by_occurrences(inspects),
-        SortBy::None => inspects,
-    }
+  match by {
+    SortBy::Referenced => sort_by_referenced(inspects),
+    SortBy::Occurrences => sort_by_occurrences(inspects),
+    SortBy::None => inspects,
+  }
 }
 
 fn sort_by_occurrences(inspects: Vec<Inspect>) -> Vec<Inspect> {
-    let mut sorted_inspects = inspects;
-    sorted_inspects.sort_by_key(|inspect| std::cmp::Reverse(inspect.occurrences));
-    sorted_inspects
+  let mut sorted_inspects = inspects;
+  sorted_inspects.sort_by_key(|inspect| std::cmp::Reverse(inspect.occurrences));
+  sorted_inspects
 }
 
 fn sort_by_referenced(inspects: Vec<Inspect>) -> Vec<Inspect> {
-    let mut sorted_inspects = inspects;
-    sorted_inspects.sort_by_key(|inspect| std::cmp::Reverse(inspect.referenced));
-    sorted_inspects
+  let mut sorted_inspects = inspects;
+  sorted_inspects.sort_by_key(|inspect| std::cmp::Reverse(inspect.referenced));
+  sorted_inspects
 }
