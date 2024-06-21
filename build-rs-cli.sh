0bff57c91ad8ca9185435aa8a0804fcc65257889
pmqueiroz <pedroqueiroz593@gmail.com>
2024-06-21 13:59:59 -0300

    chore: delete ts codebase

--- build-rs-cli.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ./crates/inspect_cli
-
-cargo build --release
-
-cp ./target/release/inspect_cli ../../bin/inspect-cli-rs
--- src/helpers/get-import-declaration-nodes.ts
+++
@@ -1,8 +0,0 @@
-import { ModuleItem, ImportDeclaration } from '@swc/core'
-
-const importDeclarationFilter = (node: ModuleItem): node is ImportDeclaration =>
-  node.type === 'ImportDeclaration'
-
-export const getImportDeclarationNodes = (nodes: ModuleItem[]) => {
-  return nodes.filter(importDeclarationFilter)
-}
