a46d505bac0a510ac358e349eaf2b96424eb1c57
pmqueiroz <pedroqueiroz593@gmail.com>
2025-02-05 00:41:02 -0300

    chore: implement kotlin inspector basis

--- crates/inspect_kotlin/src/lib.rs
+++ crates/inspect_kotlin/src/lib.rs
@@ -1,6 +1,27 @@
+extern crate core;
 extern crate tree_sitter;
 
+use core::{Inspect, Inspector};
+use std::path::PathBuf;
 use tree_sitter::{Parser, TreeCursor};
+
+pub struct KotlinInspector;
+
+impl Inspector for KotlinInspector {
+  fn inspect(&self, content: String) -> Vec<Inspect> {
+    inspect_file(&content);
+
+    vec![]
+  }
+
+  fn get_dependencies(&self, _cwd: &PathBuf) -> Vec<String> {
+    vec![]
+  }
+
+  fn get_files(&self, _cwd: &PathBuf, _include: Vec<String>) -> Vec<String> {
+    vec![]
+  }
+}
 
 pub fn inspect_file(source_code: &str) {
   let mut parser = Parser::new();
