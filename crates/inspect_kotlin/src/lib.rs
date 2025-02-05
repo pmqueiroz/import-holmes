edda37f1f318363a05b3e84a1dcbfd5841c98377
pmqueiroz <pedroqueiroz593@gmail.com>
2025-02-05 09:33:38 -0300

    feat: inspect kotlin

--- crates/inspect_kotlin/src/lib.rs
+++ crates/inspect_kotlin/src/lib.rs
@@ -3,15 +3,16 @@ extern crate tree_sitter;
 
 use core::{glob, Inspect, Inspector};
 use std::path::PathBuf;
-use tree_sitter::{Parser, TreeCursor};
+
+mod parser;
+mod visitor;
 
 pub struct KotlinInspector;
 
 impl Inspector for KotlinInspector {
   fn inspect(&self, content: String) -> Vec<Inspect> {
-    inspect_file(&content);
-
-    vec![]
+    let tree = parser::parse(&content);
+    visitor::get_program_inspects(tree, &content)
   }
 
   fn get_modules_filter(&self, _cwd: &PathBuf) -> Vec<String> {
@@ -21,38 +22,6 @@ impl Inspector for KotlinInspector {
   fn get_files(&self, cwd: &PathBuf, include: Vec<String>) -> Vec<String> {
     get_files(cwd, include)
   }
-}
-
-fn inspect_file(source_code: &str) {
-  let mut parser = Parser::new();
-  parser
-    .set_language(&tree_sitter_kotlin::language())
-    .expect("Error loading kotlin grammar");
-  let tree = parser.parse(source_code, None).unwrap();
-
-  let root_node = tree.root_node();
-  let mut cursor = root_node.walk();
-
-  fn visit_imports(cursor: &mut TreeCursor, source_code: &str) {
-    loop {
-      let node = cursor.node();
-      if node.kind() == "import_header" {
-        let import_text = node.utf8_text(source_code.as_bytes()).unwrap();
-        println!("Found import: {}", import_text);
-      }
-
-      if cursor.goto_first_child() {
-        visit_imports(cursor, source_code);
-        cursor.goto_parent();
-      }
-
-      if !cursor.goto_next_sibling() {
-        break;
-      }
-    }
-  }
-
-  visit_imports(&mut cursor, source_code);
 }
 
 fn get_files(cwd: &PathBuf, include: Vec<String>) -> Vec<String> {
