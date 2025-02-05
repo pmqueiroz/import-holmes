c35dd3b81ff6e46b7feea947ebba30cffdec99fb
pmqueiroz <pedroqueiroz593@gmail.com>
2025-02-05 09:58:42 -0300

    feat: count referenced

--- crates/inspect_kotlin/src/visitor.rs
+++ crates/inspect_kotlin/src/visitor.rs
@@ -1,6 +1,6 @@
 use core::{Inspect, RawInspect};
 use std::collections::HashMap;
-use tree_sitter::{Tree, TreeCursor};
+use tree_sitter::{Node, Tree, TreeCursor};
 
 struct ImportVisitor {
   raw_inspects: Vec<RawInspect>,
@@ -18,27 +18,13 @@ impl ImportVisitor {
   pub fn visit_program(&mut self, cursor: &mut TreeCursor, source_code: &str) {
     loop {
       let node = cursor.node();
+
       if node.kind() == "import_header" {
-        let import_identifier = node
-          .child(1)
-          .map(|n| n.utf8_text(source_code.as_bytes()).unwrap().to_string())
-          .unwrap_or_default();
+        self.visit_import(&node, source_code);
+      }
 
-        let (module_name, specifier) =
-          split_module_specifier(import_identifier.clone());
-
-        let import_alias = node
-          .child(2)
-          .map(|n| n.utf8_text(source_code.as_bytes()).unwrap().to_string())
-          .map(|alias| alias.trim_start_matches("as ").to_string());
-
-        let raw_inspect = RawInspect {
-          specifier: specifier.clone(),
-          local_specifier: import_alias.unwrap_or(specifier),
-          module_name,
-        };
-
-        self.raw_inspects.push(raw_inspect);
+      if node.kind() == "simple_identifier" {
+        self.visit_ident(&node, source_code);
       }
 
       if cursor.goto_first_child() {
@@ -49,6 +35,38 @@ impl ImportVisitor {
       if !cursor.goto_next_sibling() {
         break;
       }
+    }
+  }
+
+  fn visit_import(&mut self, node: &Node, source_code: &str) {
+    let import_identifier = node
+      .child(1)
+      .map(|n| n.utf8_text(source_code.as_bytes()).unwrap().to_string())
+      .unwrap_or_default();
+
+    let (module_name, specifier) = split_module_specifier(import_identifier);
+
+    let import_alias = node
+      .child(2)
+      .map(|n| n.utf8_text(source_code.as_bytes()).unwrap().to_string())
+      .map(|alias| alias.trim_start_matches("as ").to_string());
+
+    let raw_inspect = RawInspect {
+      specifier: specifier.clone(),
+      local_specifier: import_alias.unwrap_or(specifier),
+      module_name,
+    };
+
+    self.raw_inspects.push(raw_inspect);
+  }
+
+  fn visit_ident(&mut self, node: &Node, source_code: &str) {
+    if let Ok(ident_text) = node.utf8_text(source_code.as_bytes()) {
+      let counter = self
+        .reference_counts
+        .entry(ident_text.to_string())
+        .or_insert(0);
+      *counter += 1;
     }
   }
 }
@@ -65,19 +83,18 @@ fn split_module_specifier(input: String) -> (String, String) {
 }
 
 pub fn get_program_inspects(tree: Tree, source_code: &str) -> Vec<Inspect> {
-  let mut inspects = Vec::new();
   let root_node = tree.root_node();
   let mut cursor = root_node.walk();
 
   let mut visitor = ImportVisitor::new();
   visitor.visit_program(&mut cursor, source_code);
 
+  let mut inspects = Vec::new();
   for raw_inspect in visitor.raw_inspects.clone() {
     let import_offset = if raw_inspect.specifier == raw_inspect.local_specifier
     {
       visitor
         .raw_inspects
-        .clone()
         .iter()
         .filter(|item| item.specifier == raw_inspect.specifier)
         .count()
@@ -88,13 +105,13 @@ pub fn get_program_inspects(tree: Tree, source_code: &str) -> Vec<Inspect> {
     let referenced = visitor
       .reference_counts
       .get(&raw_inspect.local_specifier)
-      .map_or(0, |value| value - import_offset);
+      .map_or(0, |value| value.saturating_sub(import_offset));
 
     inspects.push(Inspect {
       raw: raw_inspect,
       referenced,
       occurrences: 1,
-    })
+    });
   }
 
   inspects
